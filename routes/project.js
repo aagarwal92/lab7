7eece269ed45d79127490bb05767d4e96e5610be
Ankit <kitilicous@gmail.com>
2014-02-20 21:18:28 -0800

    putting it together lab

--- routes/project.js
+++ routes/project.js
@@ -5,6 +5,7 @@ exports.projectInfo = function(req, res) { 
 
   // query for the specific project and
   // call the following callback
+  models.Project.find({"_id" : projectID}).exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -18,11 +19,37 @@ exports.addProject = function(req, res) {
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+
+  var newProj = new models.Project ({
+    "title": form_data.project_title,
+    "image": form_data.image_url,
+    "date": form_data.date,
+    "summary": form_data.summary
+  });
+  newProj.save(afterSaving);
+
+  //from the slides
+  function afterSaving(err)
+  {
+    if (err)
+    {
+      console.log(err);
+      res.send(500);
+    }
+    res.redirect('/');
+  }
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
+  models.Project.find({"_id" : projectID}).remove().exec(afterRemoving);
+
+  function afterRemoving(err, projects) 
+  {
+    if (err) console.log(err);
+    res.send();
+  }
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
 }
